2d8771af60fc44da18a91df8ef32075e02d34c4d
hs16058922 <hs16058922@gmail.com>
2021-07-18 16:00:59 +0530

    Updates

---
+++ precache-manifest.a3c0782907574ce4a91dd966e8723d8b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0dcb23914a064c6c8821a3d7457bd6c",
+    "url": "/PARI_RESIDENCY/index.html"
+  },
+  {
+    "revision": "cc43e8b57520a97bb24a",
+    "url": "/PARI_RESIDENCY/static/css/main.285047e0.chunk.css"
+  },
+  {
+    "revision": "ed3792dd665fd274e094",
+    "url": "/PARI_RESIDENCY/static/js/2.2547ef97.chunk.js"
+  },
+  {
+    "revision": "3f0e7270d9361d86cdedbbc2d6ca185f",
+    "url": "/PARI_RESIDENCY/static/js/2.2547ef97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34d12b777950b1b28a19",
+    "url": "/PARI_RESIDENCY/static/js/3.b2c035f2.chunk.js"
+  },
+  {
+    "revision": "cc43e8b57520a97bb24a",
+    "url": "/PARI_RESIDENCY/static/js/main.0164c3a6.chunk.js"
+  },
+  {
+    "revision": "fd7c35f6c5e08b2d2ae1",
+    "url": "/PARI_RESIDENCY/static/js/runtime-main.5a1c731a.js"
+  },
+  {
+    "revision": "7928778b8669c308d9795534a99eb95c",
+    "url": "/PARI_RESIDENCY/static/media/add.7928778b.wav"
+  },
+  {
+    "revision": "693cccc6253975a734eaf65fa60c5c0e",
+    "url": "/PARI_RESIDENCY/static/media/click.693cccc6.wav"
+  },
+  {
+    "revision": "2f54099a5ddff3de4b239f9042b5e974",
+    "url": "/PARI_RESIDENCY/static/media/congrats.2f54099a.gif"
+  },
+  {
+    "revision": "9c4baa0e71d320be81c24343e8a67a46",
+    "url": "/PARI_RESIDENCY/static/media/loading.9c4baa0e.gif"
+  },
+  {
+    "revision": "83952faca7350cfff7ea97b033e53fa1",
+    "url": "/PARI_RESIDENCY/static/media/logo.83952fac.png"
+  },
+  {
+    "revision": "7d3e1786288845c19ef7b2fdd29b94da",
+    "url": "/PARI_RESIDENCY/static/media/power_up.7d3e1786.mp3"
+  },
+  {
+    "revision": "f840620d9a33ab579227945a28f32fff",
+    "url": "/PARI_RESIDENCY/static/media/prototype.f840620d.jpg"
+  }
+]);
